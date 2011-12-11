b2d297070b1ff1926644e3da78378216c6cef597
John Giannelos <johngiannelos@gmail.com>
2011-12-11 22:53:19 +0200

    Fixed input reader

--- books.c
+++ books.c
@@ -1,37 +1,47 @@
 #define max(x1,x2) ((x1) > (x2) ? (x1):(x2))
 #include <stdio.h>
 #include <stdlib.h>
-/* 
-   The Partition Problem
-   aka: "The book-copy problem"
-   Author: John Giannelos <johngiannelos@gmail.com>
-*/
 
+int main(void){
+  int i,j,x,k,s,n,pages;
+  int *S,*p,**M,**D;
+  
+  scanf("%d",&n);
+  scanf("%d",&k);
+  
+  S=(int*)malloc(n*sizeof(int));
+  p=(int*)malloc((n+1)*sizeof(int));
+  M=malloc(n*sizeof(int *));
+  D=malloc(n*sizeof(int *));
+  
+  for (i=0;i<n;i++){
+    M[i]=malloc(k*sizeof(int));
+    D[i]=malloc(k*sizeof(int));
+  }
 
-int main(void) {
-  
-  //Input array & partition number example:
-  int S[9]={1,2,3,4,5,6,7,8,9};
-  int k=3,i,j,x,s;
-  int p[10],M[9][3],D[9][3];
-  p[0]=0;
-  
+  i=0;
+  while (scanf("%d", &pages) == 1) {
+    S[i]=pages;
+    i++;
+  }
+   
   //Computing prefix sums
-  for (i=0;i<9;i++){
+  p[0]=0;
+  for (i=0;i<n;i++){
     p[i+1]=p[i]+S[i];
   }
-  
+
   //Initialize boundary conditions
-  for (i=0;i<9;i++){
+  for (i=0;i<n;i++){
     M[i][0]=p[i+1];
   }
-  for (i=0;i<3;i++){
+  for (i=0;i<k;i++){
     M[0][i]=S[0];
   }
   
   //Computing dynamic programming matrices M and D
-  for (i=1;i<9;i++){
-    for (j=1;j<3;j++){
+  for (i=1;i<n;i++){
+    for (j=1;j<k;j++){
       M[i][j]=99999;
       for (x=0;x<(i-1);x++){
 	s=max(M[x][j-1],p[i]-p[x]);
@@ -41,14 +51,15 @@ int main(void) {
 	}
       }
     }
-  }
-  
+  }  
   
   for (i=0;i<9;i++){
     for (j=0;j<3;j++){
-      printf("%d ",D[i][j]);
+      printf("%d ",M[i][j]);
     }
     printf("\n");
   }
+
   return 0;
+
 }
