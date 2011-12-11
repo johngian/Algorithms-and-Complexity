e1d9d8db8e9f0dbb6599896c2966d2625d562b85
John Giannelos <johngiannelos@gmail.com>
2011-12-11 23:39:01 +0200

    First correct output/insane indexing

--- books.c
+++ books.c
@@ -3,18 +3,18 @@
 #include <stdlib.h>
 
 int main(void){
-  int i,j,x,k,s,n,pages;
-  int *S,*p,**M,**D;
+  int i,j,x,k,s,n,pages,tmp1,tmp2;
+  int *S,*p,prev,**M,**D;
   
   scanf("%d",&n);
   scanf("%d",&k);
   
   S=(int*)malloc(n*sizeof(int));
   p=(int*)malloc((n+1)*sizeof(int));
-  M=malloc(n*sizeof(int *));
-  D=malloc(n*sizeof(int *));
+  M=malloc((n+1)*sizeof(int *));
+  D=malloc((n+1)*sizeof(int *));
   
-  for (i=0;i<n;i++){
+  for (i=0;i<n+1;i++){
     M[i]=malloc(k*sizeof(int));
     D[i]=malloc(k*sizeof(int));
   }
@@ -27,24 +27,27 @@ int main(void){
    
   //Computing prefix sums
   p[0]=0;
-  for (i=0;i<n;i++){
-    p[i+1]=p[i]+S[i];
+  for (i=1;i<=n;i++){
+    p[i]=p[i-1]+S[i-1];
+    
   }
 
   //Initialize boundary conditions
-  for (i=0;i<n;i++){
-    M[i][0]=p[i+1];
+  for (i=1;i<=n;i++){
+    M[i][1]=p[i];
   }
-  for (i=0;i<k;i++){
-    M[0][i]=S[0];
+  for (i=0;i<=k;i++){
+    M[1][i]=S[0];
   }
   
   //Computing dynamic programming matrices M and D
-  for (i=1;i<n;i++){
-    for (j=1;j<k;j++){
-      M[i][j]=99999;
-      for (x=0;x<(i-1);x++){
-	s=max(M[x][j-1],p[i]-p[x]);
+  for (i=2;i<=n;i++){
+    for (j=2;j<=k;j++){
+      M[i][j]=999999;
+      for (x=1;x<=(i-1);x++){
+	tmp1=M[x][j-1];
+	tmp2=p[i]-p[x];
+	s=max(tmp1,tmp2);
 	if (M[i][j]>s){
 	  M[i][j]=s;
 	  D[i][j]=x;
@@ -53,12 +56,22 @@ int main(void){
     }
   }  
   
-  for (i=0;i<9;i++){
-    for (j=0;j<3;j++){
+  for (i=1;i<=n;i++){
+    for (j=1;j<=k;j++){
       printf("%d ",M[i][j]);
     }
     printf("\n");
   }
+  printf("\n");
+
+  for (i=1;i<=n;i++){
+    for (j=1;j<=k;j++){
+      printf("%d ",D[i][j]);
+    }
+    printf("\n");
+  }
+  printf("\n");
+
 
   return 0;
 
