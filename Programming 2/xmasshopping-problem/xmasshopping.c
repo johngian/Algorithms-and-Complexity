b9b084881bf61606a00ec6b475c011c57654f412
John Giannelos <johngiannelos@gmail.com>
2012-01-04 01:54:15 +0200

    Finished input reading

--- Programming 2/xmasshopping-problem/xmasshopping.c
+++ Programming 2/xmasshopping-problem/xmasshopping.c
@@ -3,7 +3,33 @@
 #include <stdlib.h>
 
 int main(void){
+  
+  int i,j,tmp;
+  int n,r,c;  
+  int Ax,Ay,Bx,By;
+  int shop_x,shop_y, **shops;
+  
+  //Memory allocation
+  shops=malloc(n*sizeof(int));
+  for (i=0;i<n;i++){
+    shops[i]=malloc(2*sizeof(int));
+  }
+  
+  //Input reading
+  scanf("%d %d %d\n",&n,&r,&c);
+  scanf("%d",&Ax);
+  scanf("%d",&Ay);
+  scanf("%d",&Bx);
+  scanf("%d",&By);
+  
+  i=0;
+  while (scanf("%d",&shop_x)==1){
+    scanf("%d",&shop_y);
+    shops[i][0]=shop_x;
+    shops[i][1]=shop_y;
+    i++;
+  }
 
 
-    return 0;
+  return 0;
 }
