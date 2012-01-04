1391565e842204d67a2e9e6b1f4b7385d91fab0e
John Giannelos <johngiannelos@gmail.com>
2012-01-04 20:55:41 +0200

    Initial approach to dynamic programming recursion

--- Programming 2/xmasshopping-problem/xmasshopping.c
+++ Programming 2/xmasshopping-problem/xmasshopping.c
@@ -2,9 +2,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int manhattan(int **xy,int i,int j){
+  return (abs(xy[i][0]-xy[j][0])+abs(xy[i][1]-xy[j][1]));
+}
+
 int main(void){
   
-  int i,j,tmp;
+  int i,j,k,min_dist,tmp;
   int n,r,c;  
   int Ax,Ay,Bx,By;
   int shop_x,shop_y, **shops;
@@ -28,6 +32,39 @@ int main(void){
     shops[i][0]=shop_x;
     shops[i][1]=shop_y;
     i++;
+  }
+
+  //Problem solution
+  //dtable: nxn dynamic array
+  
+  int **dtable;
+  dtable=malloc(n*sizeof(int));
+  for (i=0;i<n;i++){
+    dtable[i]=malloc(n*sizeof(int));
+  }
+  
+  for (i=0;i<n;i++){
+    dtable[0][i]=0;
+  }
+  
+  //Dynamic programming recursion
+  for (i=1;i<n;i++){
+    for (j=i+1;j<n;j++){
+      if (j-i==1){
+	min_dist=dtable[0][i];
+	k=0;
+	for (tmp=1;tmp<j;tmp++){
+	  if (dtable[tmp][i]<min_dist){
+	    min_dist=dtable[tmp][i];
+	    k=tmp;
+	  }
+	}
+	dtable[i][j]=min_dist+manhattan(shops,k,j);
+      }
+      else{
+	dtable[i][j]=dtable[i][j-1]+manhattan(shops,j-1,j);
+      }
+    }
   }
 
 
