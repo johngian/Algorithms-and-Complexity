f2c9f7856d695be0b0130dbf5ef32e8bbd1268c6
John Giannelos <johngiannelos@gmail.com>
2012-01-05 17:23:54 +0200

    Segmentation fault still not found the line

--- Programming 2/xmasshopping-problem/xmasshopping.c
+++ Programming 2/xmasshopping-problem/xmasshopping.c
@@ -2,13 +2,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
 int manhattan(int **xy,int i,int j){
-  return (abs(xy[i][0]-xy[j][0])+abs(xy[i][1]-xy[j][1]));
+  if (i==0){
+    return 0;
+  }
+  else{
+    return (abs(xy[i-1][0]-xy[j-1][0])+abs(xy[i-1][1]-xy[j-1][1]));
+  }
 }
+
 
 int main(void){
   
-  int i,j,k,min_dist,tmp,min;
+  int i,j,tmp,min;
   int n,r,c;  
   int Ax,Ay,Bx,By;
   int shop_x,shop_y, **shops;
@@ -35,42 +42,47 @@ int main(void){
   }
 
   //Problem solution
-  //dtable: nxn dynamic array
+  //dtable: (n+1)x(n+1) dynamic array
   int **dtable;
-  dtable=malloc(n*sizeof(int *));
-  for (i=0;i<n;i++){
-    dtable[i]=malloc(n*sizeof(int));
+  dtable=malloc((n+1)*sizeof(int *));
+  for (i=0;i<(n+1);i++){
+    dtable[i]=malloc((n+1)*sizeof(int));
   }
   
   //dtable[i][j] initialization
-  for (i=0;i<n;i++){
-    for (j=0;j<n;j++){
+  for (i=0;i<n+1;i++){
+    for (j=0;j<n+1;j++){
       dtable[i][j]=0;
     }
   }
   
+
   //Dynamic programming recursion
-  for (i=0;i<n-1;i++){
-    for (j=i+1;j<n;j++){
-      if (j-i==1){
-	min_dist=dtable[0][i];
-	k=0;
-	for (tmp=1;tmp<j;tmp++){
-	  if (dtable[tmp][i]<min_dist){
-	    min_dist=dtable[tmp][i];
-	    k=tmp;
-	  }
-	}
-	dtable[i][j]=min_dist+manhattan(shops,k,j);
-	printf("%d \n",dtable[i][j]);
+  
+  for (i=0;i<n;i++){
+    for (j=i+1;j<n+1;j++){
+      if (i==0 && j==1){
+	dtable[i][j]=0;
       }
       else{
-	dtable[i][j]=dtable[i][j-1]+manhattan(shops,j-1,j);
-	printf("%d \n",dtable[i][j]);
+	if (j==i-1){
+	  min=dtable[0][i]+manhattan(shops,0,j);
+	  for (tmp=1;tmp<i;tmp++){
+	    if ((dtable[tmp][i]+manhattan(shops,tmp,j))<min){
+	      min=dtable[tmp][i]+manhattan(shops,tmp,j);
+	    }
+	    dtable[i][j]=min;
+	  }
+	}
+	else{
+	  dtable[i][j]=dtable[i][j-1]+manhattan(shops,j-1,j);
+	}
       }
     }
   }
+  
 
+  
   min=dtable[0][n-1];
   for (i=1;i<n;i++){
     if (dtable[i][n-1]<min){
@@ -78,6 +90,6 @@ int main(void){
     }
     printf("%d \n",min);
   }
-
+  
   return 0;
 }
