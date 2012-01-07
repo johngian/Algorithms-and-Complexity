9d7cfee7ada2337545e0e317e30d0fed3f7b7bba
John Giannelos <johngiannelos@gmail.com>
2012-01-07 13:49:55 +0200

    Finished input reading teleport

--- Programming 2/teleport-problem/teleport.c
+++ Programming 2/teleport-problem/teleport.c
@@ -1,9 +1,24 @@
-#define max(x1,x2) ((x1) > (x2) ? (x1):(x2))
 #include <stdio.h>
 #include <stdlib.h>
 
 int main(void){
+  int i,Ai,Bi;
+  
+  //Input reading
+  int n,**tele;
+  scanf("%d",&n);
+  
+  tele=malloc(n*sizeof(int *));
+  for (i=0;i<n;i++){
+    tele[i]=malloc(2*sizeof(int));
+  }
+  
+  for (i=0;i<n;i++){
+    scanf("%d %d",&Ai,&Bi);
+    tele[i][0]=Ai;
+    tele[i][1]=Bi;
+  }
 
 
-    return 0;
+  return 0;
 }
