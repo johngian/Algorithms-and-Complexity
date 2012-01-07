3d91bbeabc6c8de282d30347decfc2568dde30f6
John Giannelos <johngiannelos@gmail.com>
2012-01-07 20:57:03 +0200

    Fixed tele[i][j] sorting

--- Programming 2/teleport-problem/teleport.c
+++ Programming 2/teleport-problem/teleport.c
@@ -1,6 +1,51 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void swap(int *x,int *y)
+{
+   int temp;
+   temp = *x;
+   *x = *y;
+   *y = temp;
+}
+
+int choose_pivot(int i,int j )
+{
+   return((i+j) /2);
+}
+
+void quicksort(int *list,int m,int n,int *b)
+{
+   int key,i,j,k;
+   if( m < n)
+   {
+      k = choose_pivot(m,n);
+      swap(&list[m],&list[k]);
+      swap(&b[m],&b[k]);
+      key = list[m];
+      i = m+1;
+      j = n;
+      while(i <= j)
+      {
+         while((i <= n) && (list[i] <= key))
+                i++;
+         while((j >= m) && (list[j] > key))
+                j--;
+         if( i < j){
+                swap(&list[i],&list[j]);
+		swap(&b[i],&b[j]);
+	 }
+      }
+	  // swap two elements
+      swap(&list[m],&list[j]);
+      swap(&b[m],&b[j]);
+	  // recursively sort the lesser list
+      quicksort(list,m,j-1,b);
+      quicksort(list,j+1,n,b);
+   }
+}
+
+
 int mySearch(int *sorted_list, int high, int element) {
   int low=0;
   while (low <= high) {
@@ -20,21 +65,11 @@ int comp1 (const void * a, const void * b)
   return ( *(int*)a - *(int*)b );
 }
 
-int comp2 (const void * A, const void * B)
-{
-  int a[2],b[2];
-  a[0]=*(int*)A;
-  a[1]=*(int*)(A+1);
-  b[0]=*(int*)B;
-  b[1]=*(int*)(B+1);
-  return ((a[0]<b[0]) || (a[0]==b[0] && a[1]<b[1]));
-}
-
 int main(void){
   int i,Ai,Bi;
   
   //Input reading
-  int n,**tele;
+  int n,**tele,*temp1,*temp2;
   scanf("%d",&n);
   
   tele=malloc(n*sizeof(int *));
@@ -50,13 +85,17 @@ int main(void){
 
   //Solution part 
   int *ends,*x,*unique,tmp;
-  int k;
+  int k,j;
   ends=malloc(n*sizeof(int));
   unique=malloc(n*sizeof(int));
   x=malloc(n*sizeof(int));
+  temp1=malloc(n*sizeof(int));
+  temp2=malloc(n*sizeof(int));
 
   for (i=0;i<n;i++){
     ends[i]=tele[i][1];
+    temp1[i]=tele[i][0];
+    temp2[i]=tele[i][1];
   }
 
   qsort(ends,n,sizeof(int),comp1);
@@ -64,11 +103,30 @@ int main(void){
     printf("%d\n",ends[i]);
   }
   printf("\n");
-  qsort(tele,n,sizeof(tele[0]),comp2);
-  for(i=0;i<n;i++){
-    printf("%d %d\n",tele[i][0],tele[i][1]);
-  }  
+  quicksort(temp1,0,n-1,temp2);
+  
+  k=0;
+  unique[0]=temp1[0];
+  j=0;
+  printf("%d\n",unique[0]);
+  for (i=1;i<n;i++){
+    if (unique[k]!=temp1[i]){
+      k++;
+      unique[k]=temp1[i];
+      quicksort(temp2,j,i-1,temp1);
+      printf("%d\n",unique[k]);
+      j=i;
+    }
+  }
   printf("\n");
+
+  for(i=0;i<n;i++){
+    tele[i][0]=temp1[i];
+    tele[i][1]=temp2[i];
+
+  }  
+  free(temp1);
+  free(temp2);
 
   //Computing unique ends
   k=0;
@@ -91,7 +149,7 @@ int main(void){
   
   //Search for longest increasing subsequence of X(i)
   
-  int *best, *prev, j, max = 0;
+  int *best, *prev, max = 0;
   best = (int*) malloc ( sizeof( int ) * k );
   prev = (int*) malloc ( sizeof( int ) * k );
  
